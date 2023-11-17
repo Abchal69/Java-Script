c60b5b9fa30e6163181e5c682da20cf5780ca614
Abchal69 <149424158+Abchal69@users.noreply.github.com>
2023-11-17 15:31:02 -0500

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,24 @@
+function setup()  
+{   
+  createCanvas(400, 400);   
+  strokeWeight(4); 
+}  
+function draw()  
+{ 
+  background(125, 255, 125);    
+  
+    fill(255, 192, 203);    
+  
+    // legs 
+  rect(125, 250, 25, 125); 
+  rect(250, 250, 25, 125);
+    // body
+  ellipse(200, 200, 250, 200); // circle  
+  // ears 
+  // face 
+  ellipse(200, 200, 100); // circle
+  // left eye
+  // right eye    
+  // nose    
+  // mouth  
+}
